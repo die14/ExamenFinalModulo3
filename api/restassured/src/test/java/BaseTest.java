ed7a2a2be21e7d0edc8ba3556a015f48ec7f8af9
die14 <66191031+die14@users.noreply.github.com>
2020-11-03 23:27:48 -0600

    .

--- api/restassured/src/test/java/BaseTest.java
+++ api/restassured/src/test/java/BaseTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class BaseTest {
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.*;
+/*
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+*/
+
+public class BaseTest {
+
+    public RequestSpecification request;
+
+    @BeforeClass
+    @Parameters ("baseUrl")
+    public void SetupRestAssured(@Optional String baseUrl){
+        if (baseUrl ==null){
+            baseUrl = "http://localhost:9000";
+        }
+        RestAssured.baseURI = baseUrl;
+
+        System.out.println(String.format("Running on %s environment", baseUrl));
+    }
+
+    @BeforeMethod
+    public void before(){
+        request = RestAssured.given();
+    }
+
 }
--- api/restassured/src/test/java/PingTests.java
+++ api/restassured/src/test/java/PingTests.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class PingTests {
+import org.testng.annotations.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PingTests extends BaseTest{
+
+    private static final String RESOURCE = "/ping";
+
+    @Test
+    public void Ping_Tests(){
+        request
+                .get(String.format("%s", RESOURCE))
+                .then()
+                .statusCode(200)
+                .header("Access-Control-Allow-Origin", equalTo("http://localhost"))
+                .body("id", equalTo(3),
+                        "response", equalTo("pong"));
+
+    }
+
+    //hay q cambiar examen.testng.xml el nombre de este archivo q es el q tiene q correr
 }
