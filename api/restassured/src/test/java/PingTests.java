332ab1e82bf7feaae0bf01cb03248243d7b94894
die14 <66191031+die14@users.noreply.github.com>
2020-11-04 23:20:58 -0600

    .

--- api/restassured/src/test/java/PingTests.java
+++ api/restassured/src/test/java/PingTests.java
@@ -1,11 +1,14 @@
 import org.testng.annotations.Test;
-
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class PingTests extends BaseTest{
 
-    private static final String RESOURCE = "/ping";
-
+    //private static final String RESOURCE = "/ping";
+    private static final String statusURL = "/v1/examen/status";
+    private static final String randomNameURL = "/v1/examen/randomName";
+    private static final String sameNameURL = "/v1/examen/sameName";
+    private static final String updateNameURL = "/v1/examen/updateName";
+/*
     @Test
     public void Ping_Tests(){
         request
@@ -15,8 +18,44 @@ public class PingTests extends BaseTest{
                 .header("Access-Control-Allow-Origin", equalTo("http://localhost"))
                 .body("id", equalTo(3),
                         "response", equalTo("pong"));
+    }
+ */
 
+    @Test
+    public void Status_Tests(){
+        request
+                .get(String.format("%s", statusURL))
+            .then()
+                .statusCode(200);
     }
 
-    //hay q cambiar examen.testng.xml el nombre de este archivo q es el q tiene q correr
+        @Test
+    public void randomName_Tests(){
+        request
+                .auth()
+                .preemptive()
+                .basic("testuser", "testpass")
+                .get(String.format("%s", randomNameURL))
+            .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void sameName_Tests(){
+        request
+                .body("{\"name\":\"Stone Cold\"}")
+                .post(String.format("%s", sameNameURL))
+            .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void updateName_Tests(){
+        request
+                .body("{\"name\":\"Stone Cold\"}")
+                .put(String.format("%s", updateNameURL))
+            .then()
+                .statusCode(200);
+    }
+
 }
